9d0823e3a56c14299317378bf9fd4855a8a98efe
Aakashgupta <gameweb94@gmail.com>
2026-05-03 00:40:43 +0530

    Update frontend routes and API services

--- Frontend/src/features/auth/services/auth.api.js
+++ Frontend/src/features/auth/services/auth.api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 const api=axios.create(
     {
-        baseURL:'http://localhost:5004',
+        baseURL:'https://interviewreportgeneratorapp-1.onrender.com',
         withCredentials:true
     }
 )
